8870e56f1e302f09ce534a8318fa1b536e80f8ee
indiciez <justinmajura@gmail.com>
2022-08-25 13:48:19 +0300

    vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'indiciez/tuis',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ebb8167422f01e21644b2895a97a608e42f82a58',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'indiciez/tuis' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ebb8167422f01e21644b2895a97a608e42f82a58',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/dotenv' => array(
+            'pretty_version' => 'v6.1.0',
+            'version' => '6.1.0.0',
+            'reference' => '568c11bcedf419e7e61f663912c3547b54de51df',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/dotenv',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
